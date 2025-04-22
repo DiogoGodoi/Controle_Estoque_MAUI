481e0d3ba187300860203ad1247b402310379d27
TI Ashby <ti@ashby.com.br>
2025-04-22 15:58:23 -0300

    Atualização -> hybrid -> MAUI

---
+++ Estoque.Apresentacao/Estoque.Maui/Estoque.Apresentacao.Maui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Estoque.Apresentacao.Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
